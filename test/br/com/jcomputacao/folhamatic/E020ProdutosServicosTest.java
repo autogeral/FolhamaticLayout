683728afcae5c00aadcf0b2550fe9eeb7a99f963
Murilo Tuvani <murilo.tuvani@gmail.com>
2023-04-02 11:40:24 -0300

    Update E020ProdutosServicosTest.java

--- test/br/com/jcomputacao/folhamatic/E020ProdutosServicosTest.java
+++ test/br/com/jcomputacao/folhamatic/E020ProdutosServicosTest.java
@@ -29,7 +29,19 @@ public class E020ProdutosServicosTest {
         LineModel lm = e.createModel();
         lm.setFieldValue(E020ProdutosServicos.CODIGO_PRODUTO_SERVICO, "000001A");
         lm.setFieldValue(E020ProdutosServicos.DESCRICAO, "Produto Bananinha");
-        
+        lm.setFieldValue(E020ProdutosServicos.GENERO, "86");
+        lm.setFieldValue(E020ProdutosServicos.TIPO, "00");
+        lm.setFieldValue(E020ProdutosServicos.TIPO_INVENTARIO, 1l);
+        String ncm = "8899.88.99EX99";
+        lm.setFieldValue(E020ProdutosServicos.NBM, ncm);
+        lm.setFieldValue(E020ProdutosServicos.MARCA_COMERCIAL, "CHITA");
+        lm.setFieldValue(E020ProdutosServicos.CODIGO_UNIDADE_ESTOQUE, 1);
+        lm.setFieldValue(E020ProdutosServicos.CODIGO_UNIDADE_COMERCIAL, 1);
+        lm.setFieldValue(E020ProdutosServicos.IPI_ALIQUOTA, 5d);
+        lm.setFieldValue(E020ProdutosServicos.IPI_SITUACAO_TRIBUTARIA, "99");
+        lm.setFieldValue(E020ProdutosServicos.ICMS_ALIQUOTA, 18d);
+        lm.setFieldValue(E020ProdutosServicos.ICMS_ALIQUOTA_ADICIONAL, 0d);
+        lm.setFieldValue(E020ProdutosServicos.CEST, "0199900");
         String line = lm.getRepresentation().toString();
         System.out.println("\"" + line + "\"");
     }
